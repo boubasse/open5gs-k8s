3ddfab57b76aab46912bee65c8f7e421fd3d387a
Alexis Falempin <alexis.falempin@irt-saintexupery.com>
2023-12-11 09:24:04 +0100

    Add script for builiding docker images

---
+++ docker/build-images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+is_base_not_built=$(docker image ls | grep open5gs-base)
+
+if [ -z "$is_base_not_built" ]
+then
+    docker build -t open5gs-base:latest base/
+fi
+
+for image_dir in */
+if [$image_dir != "base"]
+then
+    do
+        echo "Building ${image_dir} ..."
+        docker build -q -t open5gs-${image}:latest $image_dir/
+        echo "Image has been successfully built"
+    done
+fi
